e8dbde5e871dc80c50d15e3b3dfd496bd2fe74d2
George Lima <georgelima11@hotmail.com>
2016-04-06 12:06:01 -0300

    up exceptions

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict'
-module.exports = function(nome){
+module.exports = function(nome, nomeExec){
   //DEPENDENCIES
   const fs = require('fs');
   const childProcess = require('child_process');
@@ -9,20 +9,45 @@ module.exports = function(nome){
   if (typeof nomeArquivo !== 'string'){
     nomeArquivo = '';
     console.log('Nome do arquivo não informado!');
+    process.exit(0);
   }
+
+  //TESTA SE ARQUIVO EXISTE, SE NÃO PUDER SER LIDO, FINALIZA O PROCESSO
+   try{
+    fs.accessSync(nomeArquivo, fs.R_OK);
+   }catch(e){
+     console.error('Arquivo não encontrado!');
+     process.exit(1);
+   }
+
   //GERA O NOME EXECUTAVEL A PARTIR DO VALOR INFORMADO NA LINHA DE COMANDO OU RETIRANDO .C DO NOME DO ARQUIVO
   const nomeExecutavel = process.argv[3] || nomeArquivo.replace('.c', '') || '';
   //INICIA O PROGRAMA
   console.log('==== Recompilador C ====');
-  console.log(`==== Assistindo por mudanças no arquivo ${nomeArquivo} ====`);
+
+  var query = `gcc -Wall -o ${nomeExecutavel} ${nomeArquivo}`;
+
+  //INICIA COMPILANDO
+  childProcess.exec(query, (err, stdout, stderr)=> {
+    //SE HOUVER ERRO NA COMPILAÇÃO, LANÇA O ERRO!
+    if (err){
+      throw err;
+    }
+    //COMPILA O ARQUIVO E LANÇA O SUCESSO!
+    console.log('Compilado com Sucesso!', new Date());
+    if (stdout){
+      console.log(stdout);
+    }
+  });
 
   //ASSISTE POR MUDANÇAS NO ARQUIVO
+  console.log(`==== Assistindo por mudanças no arquivo ${nomeArquivo} ====`);
   fs.watchFile(nomeArquivo, () => {
       //GERA O SCRIPT
       let query = `gcc -Wall -o ${nomeExecutavel} ${nomeArquivo}`;
       //EXECUTA O GCC COM OS DADOS INFORMADOS
       childProcess.exec(query, (err, stdout, stderr)=> {
-        console.log(`Recompilando ${nomeArquivo}`);
+        console.log(`\nRecompilando ${nomeArquivo}`);
         //SE HOUVER ERRO NA COMPILAÇÃO, LANÇA O ERRO!
         if (err){
           throw err;
@@ -33,5 +58,8 @@ module.exports = function(nome){
           console.log(stdout);
         }
       });
+  });
+  process.on('uncaughtException', (err) => {
+    console.error(err);
   });
 }
